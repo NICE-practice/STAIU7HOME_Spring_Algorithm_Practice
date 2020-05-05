9a5df0a82e9f23eda343c3a735df82d5824c0470
Irina <61385853+kpirap18@users.noreply.github.com>
2020-05-05 16:40:24 +0300

    Update NICE_num63rs.c

--- NICE_num63rs.c
+++ NICE_num63rs.c
@@ -3,14 +3,14 @@ int numbers_game(int min, int max)
     int number = min;
     for (int i = min + 1; i < max; ++i)
     {
-        int numer_process = number, i_process = i;
-        while(numer_process && i_process)
-            if (numer_process < i_process)
-                i_process %= numer_process;
+        int number_process = number, i_process = i;
+        while(number_process && i_process)
+            if (number_process < i_process)
+                i_process %= number_process;
             else
-                numer_process %= i_process;
-        int k = (numer_process == 0) ? i_process : numer_process;
-        number = (number / k) * i;
+                number_process %= i_process;
+        int gcd_num_i = (number_process == 0) ? i_process : number_process;
+        number = (number / gcd_num_i) * i;
     }
     return number;
 }
