19f55b10a54f16281ed019c9e59503ee6fefd268
Irina <61385853+kpirap18@users.noreply.github.com>
2020-05-05 21:42:03 +0300

    Update NICE_7equeence.c

--- NICE_7equeence.c
+++ NICE_7equeence.c
@@ -6,22 +6,31 @@
 long long sequence_game(int array[])
 {
     long long number = 1;
+    
     for (int i = 0; i < ARG; i++)
         number = number * array[i];
+    
     long long number_additional = number;
+    
     for (int i = ARG; i < LEN_ARRAY; i++)
     {
         if (array[i - ARG] == 0)
         {
             number_additional = 1;
+            
             for (int j = i - (ARG - 1); j < i; j++)
                 number_additional = number_additional * array[j];
         }
         else
+        {
             number_additional = number_additional / array[i - ARG];
+        }
+        
         number_additional *= array[i];
+        
         if (number_additional > number)
             number = number_additional;
     }
+    
     return number;
 }
