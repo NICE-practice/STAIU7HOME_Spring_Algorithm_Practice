aacc4376f80bb5418cc92441931eeb592ded4dfa
zaa19u450 <64847038+zaa19u450@users.noreply.github.com>
2020-05-07 02:47:40 +1000

    Update and rename NICE_num63rs.h to nice_num63rs.h

--- NICE_num63rs.h
+++
@@ -1,7 +0,0 @@
-#ifndef _NICE_NUM63RS_H_
-#define _NICE_NUM63RS_H_
-
-#include <stdio.h>
-int numbers_game(int min, int max);
-
-#endif
---
+++ nice_num63rs.h
@@ -0,0 +1,6 @@
+#ifndef NICE_NUM63RS_H
+#define NICE_NUM63RS_H
+
+int numbers_game(int min, int max);
+
+#endif
