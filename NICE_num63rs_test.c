25e37bf69a5cebfb684e572e6e068bac5da50fce
zaa19u450 <64847038+zaa19u450@users.noreply.github.com>
2020-05-07 02:49:36 +1000

    Update NICE_num63rs_test.c

--- NICE_num63rs_test.c
+++ NICE_num63rs_test.c
@@ -1,29 +1,52 @@
-#include "NICE_num63rs.h"
+#include "nice_num63rs.h"
+#include <stdio.h>
 
 int main(void)
 {
-    int test_border_1_5_60 = numbers_game(1, 5);
-    if (test_border_1_5_60 == 60)
-        printf("Test 1: succes\n");
-    else
-        printf("Test 1: fail\n");
+    int successful_tests = 0;
 
-    int test_min_isequal_max_3_3_3 = numbers_game(3, 3);
-    if (test_min_isequal_max_3_3_3 == 3)
-        printf("Test 2: succes\n");
+    int result1 = numbers_game(1, 5);
+    if (result1 == 60)
+    {
+        successful_tests++;
+    }
     else
-        printf("Test 2: fail\n");
+    {
+        printf("Test borders failed\n");
+    }
 
-    int test_min_is1less_max_10_11_110 = numbers_game(10, 11);
-    if (test_min_is1less_max_10_11_110 == 110)
-        printf("Test 3: succes\n");
+    int result2 = numbers_game(3, 3);
+    if (result2 == 3)
+    {
+        successful_tests++;
+    }
     else
-        printf("Test 3: fail\n");
+    {
+        printf("Test min is equal to max failed\n");
+    }
 
-    int test_usual_1_10_2520 = numbers_game(1, 10);
-    if (test_usual_1_10_2520 == 2520)
-        printf("Test 4: succes\n");
+    int result3 = numbers_game(10, 11);
+    if (result3 == 110)
+    {
+        successful_tests++;
+    }
     else
-        printf("Test 4: fail\n");
+    {
+        printf("Test min is one less than max failed\n");
+    }
+
+    int result4 = numbers_game(2, 10);
+    if (result4 == 2520)
+    {
+        successful_tests++;
+    }
+    else
+    {
+        printf("Test usual failed\n");
+    }
+
+
+    printf("%d / 4 TESTS SUCCESSFUL\n", successful_tests);
+
     return 0;
 }
