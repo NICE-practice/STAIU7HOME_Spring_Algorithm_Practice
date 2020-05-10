cd696a0488794b00be7bd9d27cf8365dd7159b5d
zaa19u450 <64847038+zaa19u450@users.noreply.github.com>
2020-05-11 00:17:05 +1000

    Update nice_xo.c

--- nice_xo.c
+++ nice_xo.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+int first_check(int arr[25], int field_size, int how);
 int xogame(char **bf, const int field_size, const char symb)
 {
     int arr[25];
     int my_marks = 0, enemys_marks = 0;
-    int turn;
+    int turn, result;
 
     for (int i = 0; i < field_size; i++)
         for (int j = 0; j < field_size; j++)
@@ -27,6 +28,7 @@ int xogame(char **bf, const int field_size, const char symb)
     else
         turn = 2;
 
+
     if (field_size == 3)
     {
         if (turn == 1)
@@ -44,44 +46,24 @@ int xogame(char **bf, const int field_size, const char symb)
             }
             if (enemys_marks == 2)
             {
-                if ((arr[8] == 1) && (arr[0] == 0))
-                    return 0;
-                if ((arr[0] == 1) && (arr[8] == 0))
-                    return 8;
-                if ((arr[6] == 1) && (arr[2] == 0))
-                    return 2;
-                if (((arr[0] == -1) && (arr[1] == -1)) || ((arr[5] == -1) && (arr[8] == -1)))
-                    return 2;
-                if (((arr[0] == -1) && (arr[3] == -1)) || ((arr[7] == -1) && (arr[8] == -1)))
-                    return 6;
-                if ((arr[0] == -1) && (arr[2] == -1))
-                    return 1;
-                if ((arr[0] == -1) && (arr[6] == -1))
-                    return 3;
-                if ((arr[2] == -1) && (arr[8] == -1))
-                    return 5;
+                result = first_check(arr, field_size, 1);
+                if (result > 0)
+                    return result;
+                result = first_check(arr, field_size, -1);
+                if (result > 0)
+                    return result;
             }
             if (enemys_marks == 3)
             {
-                if ((arr[2] == 1) && (arr[6] == 0))
-                    return 6;
-                if ((arr[2] == 0) && (arr[6] == 1))
-                    return 2;
-                if (((arr[2] == 1) && (arr[8] == 1) && (arr[5] == 0)) || ((arr[3] == 1) && (arr[4] == 1) && (arr[5] == 0)))
-                    return 5;
-                if (((arr[6] == 1) && (arr[8] == 1) && (arr[7] == 0)) || ((arr[1] == 1) && (arr[4] == 1) && (arr[7] == 0)))
-                    return 7;
-                if ((arr[0] == 1) && (arr[2] == 1) && (arr[1] == 0))
-                    return 1;
-                if (((arr[0] == 1) && (arr[6] == 1) && (arr[3] == 0)) || ((arr[4] == 1) && (arr[5] == 1) && (arr[3] == 0)))
-                    return 3;
-
+                result = first_check(arr, field_size, 1);
+                if (result > 0)
+                    return result;
+                result = first_check(arr, field_size, -1);
+                if (result > 0)
+                    return result;
                 if (arr[7] == -1)
                     return 5;
-                if (arr[3] == -1)
-                    return 1;
-                if (arr[5] == -1)
-                    return 7;
+                return 1;
             }
             if (enemys_marks == 4)
                 for (int i = 0; i < 9; i++)
@@ -109,63 +91,63 @@ int xogame(char **bf, const int field_size, const char symb)
                 }
                 else
                 {
-                    if (arr[0] + arr[1] + arr[2] == -2)
-                        for (int i = 0; i < 3; i++)
-                            if (arr[i] == 0)
-                                return i;
-                    if (arr[2] + arr[5] + arr[8] == -2)
-                        for (int i = 2; i < 9; i += 3)
-                            if (arr[i] == 0)
-                                return i;
-                    if (arr[6] + arr[7] + arr[8] == -2)
-                        for (int i = 6; i < 9; i++)
-                            if (arr[i] == 0)
-                                return i;
-                    if (arr[0] + arr[3] + arr[6] == -2)
-                        for (int i = 0; i < 7; i += 3)
-                            if (arr[i] == 0)
-                                return i;
-                    if (arr[0] + arr[2] + arr[6] + arr[8] == -2)
-                        return 1;
+                    result = first_check(arr, field_size, -1);
+                    if (result > 0)
+                        return result;
                     return 6;
                 }
             }
             else
             {
-                if ((arr[0] + arr[1] + arr[2] == 2) || (arr[0] + arr[1] + arr[2] == -2))
-                    for (int i = 0; i < 3; i++)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[2] + arr[5] + arr[8] == 2) || (arr[2] + arr[5] + arr[8] == -2))
-                    for (int i = 2; i < 9; i += 3)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[6] + arr[7] + arr[8] == 2) || (arr[6] + arr[7] + arr[8] == -2))
-                    for (int i = 6; i < 9; i++)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[0] + arr[3] + arr[6] == 2) || (arr[0] + arr[3] + arr[6] == -2))
-                    for (int i = 0; i < 9; i += 3)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[0] + arr[4] + arr[8] == 2) || (arr[0] + arr[4] + arr[8] == -2))
-                    for (int i = 0; i < 9; i += 4)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[0] + arr[4] + arr[8] == 2) || (arr[0] + arr[4] + arr[8] == -2))
-                    for (int i = 0; i < 9; i += 4)
-                        if (arr[i] == 0)
-                            return i;
-                if ((arr[2] + arr[4] + arr[6] == 2) || (arr[2] + arr[4] + arr[6] == -2))
-                    for (int i = 0; i < 9; i += 4)
-                        if (arr[i] == 0)
-                            return i;
+                result = first_check(arr, field_size, 1);
+                if (result > 0)
+                    return result;
+                result = first_check(arr, field_size, -1);
+                if (result > 0)
+                    return result;
                 for (int i = 0; i < 9; i++)
                     if (arr[i] == 0)
                         return i;
             }
-
         }
     }
     return 0;
+}
+int first_check(int arr[25], int field_size, int how)
+{
+    int result = -1;
+    int sum_column, sum_row;
+    int sum_diagonal_main = 0, sum_diagonal_bok = 0;
+
+    for (int i = 0; i < field_size; i++)
+    {
+        sum_column = 0;
+        sum_row = 0;
+        for (int j = 0; j < field_size; j++)
+        {
+            sum_column += arr[i + field_size * j];
+            sum_row += arr[i * field_size + j];
+            if (i == j)
+                sum_diagonal_main += arr[i * field_size + j];
+            if (i == field_size - 1 - j)
+                sum_diagonal_bok += arr[i * field_size + j];
+        }
+        if (sum_column == how * (field_size - 1))
+            for (int str = 0; str < field_size; str++)
+                if (arr[i + str * field_size] == 0)
+                    return i * field_size;
+        if (sum_row == how * (field_size - 1))
+            for (int col = 0; col < field_size; col++)
+                if (arr[i * field_size + col] == 0)
+                    return i * field_size + col;
+    }
+    if (sum_diagonal_main == how * (field_size - 1))
+        for (int i = 0; i < field_size; i++)
+            if (arr[i*field_size + i] == 0)
+                return i*field_size + i;
+    if (sum_diagonal_bok == how * (field_size - 1))
+        for (int i = 0; i < field_size; i++)
+            if (arr[i*field_size + (field_size - i - 1)] == 0)
+                return i*field_size + (field_size - i - 1);
+    return result;
 }
