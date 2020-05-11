29922022eabd23ea12fbd9cb55427fe757988429
Irina <61385853+kpirap18@users.noreply.github.com>
2020-05-12 00:26:18 +0300

    Create nice_xo.h

---
+++ nice_xo.h
@@ -0,0 +1,9 @@
+#ifndef _NICE_XO_H
+#define _NICE_XO_H
+
+#include <stdio.h>
+
+int first_check(int arr[25], int field_size, int how);
+int xogame(char **bf, const int field_size, const char symb);
+
+#endif // _NICE_XO_H
