f43aa85a5e7334de6dfd6aa7bf0b700cf0d3b4b8
Irina <61385853+kpirap18@users.noreply.github.com>
2020-05-15 09:54:00 +0300

    Create nice_split.c

---
+++ nice_split.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int split(const char *string, char **matrix, const char symbol)
+{
+    int number = 0;
+    int i = 0, k = 0;
+    while (string[i])
+    {
+        if (string[i] != symbol)
+        {
+            matrix[number][k] = string[i];
+            k++;
+        }
+        else if ((string[i] == symbol) && (string[i - 1] != symbol))
+        {
+            number++;
+            k = 0;
+        }
+
+        i++;
+    }
+
+    if (string[i - 1] != symbol)
+        number++;
+
+    return number;
+}
