9cbe9a76efe8af42f18f43adde307acb6bec8e19
zaa19u450 <64847038+zaa19u450@users.noreply.github.com>
2020-05-18 20:03:14 +1000

    Update nice_strtok.c

--- nice_strtok.c
+++ nice_strtok.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-int is_in_delims(const char *str, int ch);
-
 char *strtok(char *string, char const *delim)
 {
     char *pointer = NULL;
-    int i = 0;
+    int i = 0, k;
     static char *pos = NULL;
+
     if (string != NULL)
         pos = string;
     for (; pos[i]; i++)
     {
-        if (!is_in_delims(delim, pos[i]) && pointer == NULL)
+        k = 0;
+        for (int j = 0; !k && delim[j]; j++)
+            if (delim[j] == pos[i])
+                k = 1;
+        if (!k && pointer == NULL)
             pointer = &pos[i];
-        if (pointer != NULL && is_in_delims(delim, pos[i]))
+        if (k && pointer != NULL)
             pos[i--] = '\0';
     }
-    pos = &pos[++i];
+    pos = &pos[i + 1];
     return pointer;
-}
-int is_in_delims(const char *str, int ch)
-{
-    for (int i = 0; str[i]; i++)
-        if (str[i] == ch)
-            return 1;
-    return 0;
 }
